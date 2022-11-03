6266f01b1d28684a1edab3bb5aad707944572d36
mhshaw2005 <89607339+mhshaw2005@users.noreply.github.com>
2022-11-03 12:51:42 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace ConvertHex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string alpha = "Hello";
+            Converter(alpha);
+        }
+
+        static void Converter(string alpha)
+        {
+
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(alpha.ToLower());
+            var hexString = BitConverter.ToString(asciiBytes);
+            hexString = hexString.Replace("-", " ");
+
+            Console.WriteLine(hexString);
+        }
+    }
+}
